3a751d115ae2dc395500247e2d341df38e769c8b
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-30 15:33:50 -0700

    shifting stuff around

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const db_router = require('../posts/database-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Hello to the world, from Lambda School Students</h1>`);
+}); // Server running initial GET
+
+server.use('./posts', db_router);
